09582d718f7b333c05a1feaa366b98a0f8c7c314
Arturo Mendivil <arturoo1516@gmail.com>
2023-10-03 11:09:59 -0700

    fix icons beign cut and spacing

--- src/modules/FeaturedProjects/index.jsx
+++ src/modules/FeaturedProjects/index.jsx
@@ -7,7 +7,7 @@ function FeaturedProjects({ projects }) {
     <div>
       <div className='m-auto flex flex-col items-start p-5'>
         <div className='flex justify-between m-auto w-full text-left py-5'>
-          <span className='text-xl md:text-2xl 4xl:text-5xl font-semibold text-textPrimary'>My projects</span>
+          <span className='text-xl md:text-2xl 4xl:text-5xl font-semibold text-textPrimary'>Projects</span>
           <Link to='/projects' className='font-semibold text-blue-600 4xl:text-xl'>View all</Link>
         </div>
         <div className='flex flex-col w-full lg:flex-row flex-wrap gap-5 pb-5 justify-center'>
--- src/modules/SkillsSection/index.jsx
+++ src/modules/SkillsSection/index.jsx
@@ -11,19 +11,19 @@ export default function SkillsSection() {
     <div>
       <div className='m-auto flex flex-col items-start p-5'>
         <div className='flex justify-between m-auto w-full text-left py-5'>
-          <span className='text-xl md:text-2xl 4xl:text-5xl font-semibold text-textPrimary'>My Skills</span>
+          <span className='text-xl md:text-2xl 4xl:text-5xl font-semibold text-textPrimary'>Skills</span>
         </div>
         {/* <div className='flex flex-row w-full flex-wrap gap-5 pb-5 justify-center animate-infinite-scroll'> */}
         <div className='w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]'>
           <div className='flex items-center justify-center md:justify-start animate-infinite-scroll'>
             {sanitySkills.skillsIcons.map((icon) =>
-              <SkillsIcon key={icon._key} classes='text-5xl 4xl:text-6xl hover:text-cyan-500 hover:scale-[1.20] mx-5' icon={icon.icon} size={icon.size} color={icon.hexColor} />
+              <SkillsIcon key={icon._key} classes='text-5xl 4xl:text-6xl hover:text-cyan-500 hover:scale-[1.20] mx-2 md:mx-5 my-5' icon={icon.icon} size={icon.size} color={icon.hexColor} />
               )
             }
           </div>
           <div className='flex items-center justify-center md:justify-start animate-infinite-scroll-reverse'>
             {sanitySkills.skillsIcons.map((icon) =>
-              <SkillsIcon key={icon._key} classes='text-5xl 4xl:text-6xl hover:text-cyan-500 hover:scale-[1.20] mx-5' icon={icon.icon} size={icon.size} color={icon.hexColor} />
+              <SkillsIcon key={icon._key} classes='text-5xl 4xl:text-6xl hover:text-cyan-500 hover:scale-[1.20] mx-2 md:mx-5 my-5' icon={icon.icon} size={icon.size} color={icon.hexColor} />
               )
             }
           </div>
