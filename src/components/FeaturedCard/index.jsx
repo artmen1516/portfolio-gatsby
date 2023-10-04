34e516a5a3d5fc9fba740af375e4a7c417953d04
Arturo Mendivil <arturoo1516@gmail.com>
2023-10-03 19:03:13 -0700

    fix projects view in tablet

--- src/components/FeaturedCard/index.jsx
+++ src/components/FeaturedCard/index.jsx
@@ -4,13 +4,13 @@ import { Link } from 'gatsby'
 
 function FeaturedCard({ project }) {
   return (
-    <div className='max-w-md max-h-full bg-gradient-to-r from-indigo-500 to-cyan-500 rounded-xl p-0.5 mb-5'>
+    <div className='max-w-xs md:max-w-none md:w-full lg:max-w-xs 4xl:max-w-sm max-h-full bg-gradient-to-r from-indigo-500 to-cyan-500 rounded-xl p-0.5'>
       <div className='flex flex-col gap-5 mx-auto p-5 rounded-xl bg-background min-h-full items-center'>
         <Link
           to={`/projects/${project.slug.current}`}
           className='h-56 w-56 sm:h-72 sm:w-72'>
           <GatsbyImage
-            className='rounded-lg object-cover hover:scale-110 transition-all'
+            className='rounded-lg object-cover hover:scale-105 transition-all'
             alt={project.title}
             image={project.image.asset.gatsbyImageData}
           />
@@ -20,7 +20,7 @@ function FeaturedCard({ project }) {
           to={`/projects/${project.slug.current}`}
           className='rounded-lg overflow-hidden sm:flex-[1_1_0%]'>{project.title}</Link></h2>
         <div className='flex flex-row w-full m-auto gap-3 text-textSecondary 4xl:text-xl'>
-          <span className='px-3 text-textSecondary bg-dark font-bold rounded-full 4xl:text-x'>{project.year}</span>
+          <span className='px-3 text-textSecondary bg-dark font-bold rounded-full 4xl:text-xl'>{project.year}</span>
           <span className='text-textSecondary text-lg 4xl:text-xl'>{project.type.join(', ')}</span>
         </div>
         <p>{project.description}</p>
